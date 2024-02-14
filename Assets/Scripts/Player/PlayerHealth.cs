aaa30593660116b3c7ea0e6d62658581e721c70b
Tom Holewijn <Tom.holewijn@protonmail.com>
2024-02-14 10:24:09 +0100

    health works

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -8,6 +8,7 @@ public class PlayerHealth : MonoBehaviour
 
     [SerializeField] private int _maxHealth = 3;
     [SerializeField] private int _currentHealth;
+    private bool _beenHit = false;
 
     private void Start()
     {
@@ -18,15 +19,25 @@ public class PlayerHealth : MonoBehaviour
     {
         if (collision.gameObject.TryGetComponent(out BulletController l_bullet))
         {
-            ChangePlayerHealth();
+            if(!_beenHit)
+                ChangePlayerHealth();
         }
     }
 
     public void ChangePlayerHealth()
     {
-        if(_currentHealth <= 1)
+        _currentHealth--;
+        _beenHit = true;
+        StartCoroutine(HitCooldown());
+        if (_currentHealth <= 0)
+        {
             _deathText.SetActive(true);
-        else _currentHealth--;
-        
+        }
+    }
+
+    private IEnumerator HitCooldown()
+    {
+        yield return new WaitForSeconds(1);
+        _beenHit = false;
     }
 }

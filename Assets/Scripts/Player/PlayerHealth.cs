007de6c22f2c7884c3351d3ead29f11cc8540be8
Tom Holewijn <Tom.holewijn@protonmail.com>
2024-02-14 11:09:18 +0100

    damage visual

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -9,6 +9,9 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private int _maxHealth = 3;
     [SerializeField] private int _currentHealth;
     private bool _beenHit = false;
+    [SerializeField] private List<Material> _tankMaterials;
+    private List<Color> _oldMats = new List<Color>();
+
 
     private void Start()
     {
@@ -19,7 +22,7 @@ public class PlayerHealth : MonoBehaviour
     {
         if (collision.gameObject.TryGetComponent(out BulletController l_bullet))
         {
-            if(!_beenHit)
+            if (!_beenHit)
                 ChangePlayerHealth();
         }
     }
@@ -29,6 +32,7 @@ public class PlayerHealth : MonoBehaviour
         _currentHealth--;
         _beenHit = true;
         StartCoroutine(HitCooldown());
+        StartCoroutine(MaterialFlash());
         if (_currentHealth <= 0)
         {
             _deathText.SetActive(true);
@@ -39,5 +43,28 @@ public class PlayerHealth : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
         _beenHit = false;
+    }
+    private IEnumerator MaterialFlash()
+    {
+        for (int i = 0; i < _tankMaterials.Count; i++)
+        {
+            _oldMats.Add(_tankMaterials[i].color);
+        }
+        int temp = 0;
+        while (temp < 3)
+        {
+            for (int i = 0; i < _tankMaterials.Count; i++)
+            {
+                _tankMaterials[i].color = Color.red;
+
+            }
+            yield return new WaitForSeconds(0.1f);
+            for (int i = 0; i < _tankMaterials.Count; i++)
+            {
+                _tankMaterials[i].color = _oldMats[i];
+            }
+            yield return new WaitForSeconds(0.1f);
+            temp++;
+        }
     }
 }
